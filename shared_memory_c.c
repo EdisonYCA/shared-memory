2543ff79fee59905559f992245167ff618edc55e
Edison Caceres <edisonyca127@gmail.com>
2023-03-24 17:31:35 -0400

    Added code to read message in shared memory.

--- shared_memory_c.c
+++ shared_memory_c.c
@@ -7,25 +7,26 @@
 extern int errno;
 
 int main(){
-   /* Server Code */
+   printf("\nClient Executed...\n");
+   /* Client Code */
    key_t key = ftok("./memory", 1234); // generates a key for the shared memory
    int sm_id; // id for shared memory
    
-   // create share memory
-   if((sm_id = shmget(key, 100, IPC_CREAT | 0644)) < 0){
+   /* Create share memory */
+   if((sm_id = shmget(key, 100, 0)) < 0){
     perror("server: shmget");
     exit(1);
    }
 
-   // attach to the shared memory
+   /* Attach to the shared memory */
    char *address = (char *) shmat(sm_id, 0, 0);
    if(address < 0){
       perror("shared memory attach");
       exit(1);
    }
 
-   /* write a character to shared memory */
-   char* to_write = "Hello, World.";
-   strcpy(address, to_write);
-   printf("wrote value to memory: %s\n", to_write);
+   printf("Client read: %s", address);
+
+
+
 }
